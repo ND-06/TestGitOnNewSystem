0410056ba1b007f23d83ba24f2d6a92fef013f07
Natdenice <clad1987@hotmail.com>
2019-05-08 08:32:22 +0200

    Test to check git works

---
+++ test.js
@@ -0,0 +1,12 @@
+const calculate = (a,b) => {
+  return a * b;
+}
+console.log(calculate(10,9));
+
+const sort = (arr) =>  {
+  return arr.reverse();
+}
+
+const arrayNew = ["Miami", 'London', 'Paris'];
+
+console.log(sort(arrayNew));
